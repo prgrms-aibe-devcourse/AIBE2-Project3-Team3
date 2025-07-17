59e319f5e4324376efe4f837d7289b031e40861e
INUK-ai <arheon2469@gmail.com>
2025-07-17 21:05:52 +0900

    feat: BaseEntity 속성 - BaseTimeEntity 생성일, 수정일 - BaseEntity 생성자, 수정자

---
+++ ium/src/main/java/com/example/ium/_core/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.example.ium._core.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy; // 생성자
+    @LastModifiedBy
+    private String modifiedBy; // 수정자
+}
+
