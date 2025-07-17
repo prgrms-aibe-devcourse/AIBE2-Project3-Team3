2658fd6fcb2b0642ebbb757fc28f3131f53c3fb4
INUK-ai <arheon2469@gmail.com>
2025-07-17 22:36:22 +0900

    chore: Security 설정 - securityFilterChain   - WHITE_LIST   - ADMIN_LIST - CustomAuthenticationEntryPoint

---
+++ ium/src/main/java/com/example/ium/_core/config/SecurityConfig.java
@@ -0,0 +1,86 @@
+package com.example.ium._core.config;
+
+import com.example.ium._core.security.CustomAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    // 화이트리스트 정의
+    private static final String[] WHITE_LIST = {
+            "/",
+            "/error",
+            "/api/auth/**",
+            "/api/swagger-ui/**",
+            "/api/health/**",
+            "/api/actuator/**",
+            "/h2-console/**",
+            "/favicon.ico"
+    };
+
+    // 관리자 리스트 정의
+    private static final String[] ADMIN_LIST = {
+            "/api/admin/**"
+    };
+
+    /*
+        * HTTP 보안 설정
+        * 이 설정은 HTTP 요청에 대한 보안 규칙을 정의합니다.
+        * - 화이트리스트에 있는 경로는 인증 없이 접근 가능
+        * - 관리자 리스트에 있는 경로는 "ADMIN" 역할을 가진 사용자만 접근 가능
+        * - 나머지 요청은 인증된 사용자만 접근 가능
+        * - 로그인 페이지와 로그인 처리 URL을 설정
+        * - 로그아웃 시 성공 URL을 설정
+        * - HTTP 기본 인증을 사용하도록 설정
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(WHITE_LIST).permitAll()
+                        .requestMatchers(ADMIN_LIST).hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/authenticate")
+                        .usernameParameter("user")
+                        .passwordParameter("pwd")
+                        .defaultSuccessUrl("/", true)
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/"))
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+
+    // BCrypt 인코더 빈 등록
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
---
+++ ium/src/main/java/com/example/ium/_core/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.example.ium._core.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        log.error("인증되지 않은 사용자 접근: {}", authException.getMessage());
+        response.sendRedirect("/login?error=true");
+    }
+}
