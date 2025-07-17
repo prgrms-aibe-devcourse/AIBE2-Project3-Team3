6fa3c4713c3f18be62242e0894bf5613fe0e53a6
hyojin <35914102+hyojin52@users.noreply.github.com>
2025-07-17 15:12:12 +0900

    init : project setting

---
+++ ium/src/main/java/com/example/ium/IumApplication.java
@@ -0,0 +1,13 @@
+package com.example.ium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IumApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(IumApplication.class, args);
+  }
+  
+}
---
+++ ium/src/test/java/com/example/ium/IumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IumApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
