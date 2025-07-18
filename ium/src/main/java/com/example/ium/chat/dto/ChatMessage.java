464f820f9fc6882796e50ea267104de0c975bc3a
hyojin <35914102+hyojin52@users.noreply.github.com>
2025-07-18 17:11:29 +0900

    feat : chatMessage createdAt 추가

--- ium/src/main/java/com/example/ium/chat/dto/ChatMessage.java
+++ ium/src/main/java/com/example/ium/chat/dto/ChatMessage.java
@@ -14,4 +14,5 @@ public class ChatMessage {
   private String roomId; // 방번호
   private String sender; // 메시지 보낸사람
   private String message; // 메시지
+  private String createdAt;
 }
--- ium/src/main/java/com/example/ium/chat/handler/WebSockChatHandler.java
+++ ium/src/main/java/com/example/ium/chat/handler/WebSockChatHandler.java
@@ -13,6 +13,8 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -36,25 +38,28 @@ public class WebSockChatHandler extends TextWebSocketHandler {
   protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
     String payload = message.getPayload();
     ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
-    // ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
-    // Set<WebSocketSession> sessions = room.getSessions();   //방에 있는 현재 사용자 한명이 WebsocketSession
     
     // 세션 set 조회 또는 생성
     String roomId = chatMessage.getRoomId();
     Set<WebSocketSession> sessions = sessionMap.computeIfAbsent(roomId, k -> ConcurrentHashMap.newKeySet());
     
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
+    
     if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
-      //사용자가 방에 입장하면  Enter 메세지를 보내도록 해놓음.  이건 새로운사용자가 socket 연결한 것이랑은 다름.
+      //사용자가 방에 입장하면  Enter 메세지를 보내도록 해놓음. 이건 새로운사용자가 socket 연결한 것이랑은 다름.
       //socket 연결은 이 메세지 보내기전에 이미 되어있는 상태
       sessions.add(session);
       chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");  //TALK일 경우 msg가 있을 거고, ENTER일 경우 메세지 없으니까 message set
+      chatMessage.setCreatedAt(LocalDateTime.now().format(formatter));
       sendToEachSocket(sessions,new TextMessage(objectMapper.writeValueAsString(chatMessage)) );
     }else if (chatMessage.getType().equals(ChatMessage.MessageType.QUIT)) {
       sessions.remove(session);
       chatMessage.setMessage(chatMessage.getSender() + "님이 퇴장했습니다..");
+      chatMessage.setCreatedAt(LocalDateTime.now().format(formatter));
       sendToEachSocket(sessions,new TextMessage(objectMapper.writeValueAsString(chatMessage)) );
-    }else {
-      sendToEachSocket(sessions,message ); //입장,퇴장 아닐 때는 클라이언트로부터 온 메세지 그대로 전달.
+    } else {
+      chatMessage.setCreatedAt(LocalDateTime.now().format(formatter));
+      sendToEachSocket(sessions, new TextMessage(objectMapper.writeValueAsString(chatMessage))); //입장,퇴장 아닐 때는 클라이언트로부터 온 메세지 그대로 전달.
     }
   }
   private  void sendToEachSocket(Set<WebSocketSession> sessions, TextMessage message){
