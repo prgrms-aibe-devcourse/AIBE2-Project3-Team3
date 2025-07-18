b6788ce3a2c8053b9c89f5f406cbb0866de7f275
kdg_win <alsshsskssl@gmail.com>
2025-07-18 11:37:11 +0900

    feat: 메인페이지 및 인덱스 컨트롤러 추가, CSS 및 JS 파일 생성

--- ium/src/main/java/com/example/ium/index/controller/IndexController.java
+++ ium/src/main/java/com/example/ium/index/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.example.ium.mainpage.controller;
+package com.example.ium.index.controller;
 
 import com.example.ium._core.dto.Response;
 import com.example.ium._core.exception.ErrorCode;
@@ -6,21 +6,18 @@ import com.example.ium._core.exception.IumApplicationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MainpageController {
+public class IndexController {
   
   @GetMapping("/")
   public String home() {
-    return "mainpage/index";
+    return "index";
   }
   
-  @GetMapping("/mainpage")  // 혹시 몰라서 해당 경로도 매핑
-  public String mainpage() {
-    return "mainpage/index";
-  }
-  
-  @GetMapping("/mainpage/exception")
+  @GetMapping("/index/exception")
+  @ResponseBody
   public Response exception() {
     throw new IumApplicationException(ErrorCode.INTERNAL_SERVER_ERROR);
   }
