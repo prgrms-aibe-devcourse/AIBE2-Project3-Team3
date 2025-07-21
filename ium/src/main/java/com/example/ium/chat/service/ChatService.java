0f695e024e79940115bc1c91539d62471e3ac2ca
hyojin <35914102+hyojin52@users.noreply.github.com>
2025-07-21 16:05:43 +0900

    feat : 채팅방에 DB 연결

--- ium/src/main/java/com/example/ium/chat/service/ChatService.java
+++ ium/src/main/java/com/example/ium/chat/service/ChatService.java
@@ -1,8 +1,15 @@
 package com.example.ium.chat.service;
 
-import com.example.ium.chat.dto.ChatRoom;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.PostConstruct;
+import com.example.ium._core.exception.ErrorCode;
+import com.example.ium._core.exception.IumApplicationException;
+import com.example.ium.chat.domain.model.ChatMember;
+import com.example.ium.chat.domain.model.ChatMemberId;
+import com.example.ium.chat.domain.model.ChatRoom;
+import com.example.ium.chat.domain.repository.ChatMemberRepository;
+import com.example.ium.chat.domain.repository.ChatRoomRepository;
+import com.example.ium.member.domain.model.Email;
+import com.example.ium.member.domain.model.Member;
+import com.example.ium.member.domain.repository.MemberJPARepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,29 +21,35 @@ import java.util.*;
 @Service
 public class ChatService {
   
-  private final ObjectMapper objectMapper;
-  private Map<String, ChatRoom> chatRooms;
+  private final ChatRoomRepository chatRoomRepository;
+  private final ChatMemberRepository chatMemberRepository;
+  private final MemberJPARepository memberJPARepository;
   
-  @PostConstruct
-  private void init() {
-    chatRooms = new LinkedHashMap<>();
+  public List<ChatRoom> findAllRoom(String email) {
+    Member member = getMember(email);
+    List<ChatRoom> chatRoomList = chatMemberRepository.findChatRoomsByMember(member.getId());
+    return chatRoomList;
   }
   
-  public List<ChatRoom> findAllRoom() {
-    return new ArrayList<>(chatRooms.values());
+  private Member getMember(String email) {
+    return memberJPARepository.findByEmail(Email.of(email))
+            .orElseThrow(() -> new IumApplicationException(ErrorCode.MEMBER_NOT_FOUND));
   }
   
-  public ChatRoom findRoomById(String roomId) {
-    return chatRooms.get(roomId);
+  public ChatRoom findRoomById(Long roomId) {
+    return chatRoomRepository.findById(roomId)
+            .orElseThrow(() -> new IumApplicationException(ErrorCode.CHAT_ROOM_NOT_FOUND));
   }
   
-  public ChatRoom createRoom(String name) {
-    String randomId = UUID.randomUUID().toString();
-    ChatRoom chatRoom = ChatRoom.builder()
-            .roomId(randomId)
-            .name(name)
-            .build();
-    chatRooms.put(randomId, chatRoom);
-    return chatRoom;
+  public ChatRoom createRoom(String name, String email, String targetUserEmail) {
+    Member member = getMember(email);
+    Member targetMember = getMember(targetUserEmail);
+    return chatMemberRepository.findChatRoomByMembers(member.getId(), targetMember.getId())
+            .orElseGet(() -> {
+              ChatRoom newRoom = chatRoomRepository.save(ChatRoom.builder().roomName(name).build());
+              chatMemberRepository.save(new ChatMember(new ChatMemberId(newRoom, member)));
+              chatMemberRepository.save(new ChatMember(new ChatMemberId(newRoom, targetMember)));
+              return newRoom;
+            });
   }
 }
--- ium/src/main/resources/static/js/index.js
+++ ium/src/main/resources/static/js/index.js
@@ -82,6 +82,12 @@ document.querySelector('.chat-icon').addEventListener('click', function() {
     nameInput.value = getRandomString(10);
     form.appendChild(nameInput);
 
+    const targetUserInput = document.createElement('input');
+    targetUserInput.type = 'hidden';
+    targetUserInput.name = 'targetUser';
+    targetUserInput.value = 'admin@test.com'; // admin user email
+    form.appendChild(targetUserInput);
+
     document.body.appendChild(form);
     form.submit();
 });
