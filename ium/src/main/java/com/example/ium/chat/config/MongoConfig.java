301a6efc30bf9f2edef70bd8d2424e0a03fd71b5
hyojin <35914102+hyojin52@users.noreply.github.com>
2025-07-23 14:20:30 +0900

    feat: mongoDB auditing 설정 추가

--- ium/src/main/java/com/example/ium/chat/config/MongoConfig.java
+++ ium/src/main/java/com/example/ium/chat/config/MongoConfig.java
@@ -3,12 +3,14 @@ package com.example.ium.chat.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
+@EnableMongoAuditing(auditorAwareRef = "auditorProvider")
 public class MongoConfig {
   /**
    * * _class 필드가 저장되지 않도록 설정
