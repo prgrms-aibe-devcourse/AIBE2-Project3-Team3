5bf269e9bef941b6a38c57032535411368ee6a5e
hyojin <35914102+hyojin52@users.noreply.github.com>
2025-07-23 14:25:53 +0900

    feat: RepositoryConfig 위치 _core/config 로 변경

--- ium/src/main/java/com/example/ium/_core/config/RepositoryConfig.java
+++ ium/src/main/java/com/example/ium/_core/config/RepositoryConfig.java
@@ -1,4 +1,4 @@
-package com.example.ium.chat.config;
+package com.example.ium._core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
