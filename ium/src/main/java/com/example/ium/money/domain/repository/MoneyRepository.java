ed0a3ee09ebde6a99080b99e8bb4c4768f8c75de
INUK-ai <arheon2469@gmail.com>
2025-07-24 14:29:33 +0900

    feat: MoneyInfo 관련 기능 구현

--- ium/src/main/java/com/example/ium/money/domain/repository/MoneyRepository.java
+++ ium/src/main/java/com/example/ium/money/domain/repository/MoneyRepository.java
@@ -1,7 +1,16 @@
 package com.example.ium.money.domain.repository;
 
+import com.example.ium.member.domain.model.Member;
 import com.example.ium.money.domain.model.Money;
+import com.example.ium.money.domain.model.MoneyType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MoneyRepository extends JpaRepository<Money, Long> {
+
+    @Query("SELECT SUM(m.price) FROM Money m WHERE m.member = :member AND m.moneyType = :moneyType")
+    Optional<Integer> sumPriceByMemberAndMoneyType(@Param("member") Member member, @Param("moneyType") MoneyType moneyType);
 }
