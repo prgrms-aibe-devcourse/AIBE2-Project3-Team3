c6dababe1667d758658c2f9087cf62eea1db7658
INUK-ai <arheon2469@gmail.com>
2025-07-28 10:11:45 +0900

    refactor: user-report-form workRequestId 추가

---
+++ ium/src/main/java/com/example/ium/report/application/controller/ReportController.java
@@ -0,0 +1,25 @@
+package com.example.ium.report.application.controller;
+
+import com.example.ium._core.security.CustomUserDetails;
+import com.example.ium.member.application.dto.request.UserReportFormDto;
+import com.example.ium.report.domain.model.ReportReason;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/reports")
+public class ReportController {
+
+    @GetMapping("/form-data")
+    public String createUserReport(@RequestParam("workRequestId") Long workRequestId, Model model) {
+        model.addAttribute("workRequestId", workRequestId);
+        model.addAttribute("reasons", ReportReason.values());
+        return "report/user-report-form";
+    }
+}
