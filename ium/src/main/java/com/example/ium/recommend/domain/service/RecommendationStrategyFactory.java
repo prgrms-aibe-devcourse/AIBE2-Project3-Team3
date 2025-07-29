7a6e328fb26900bb69c79b57121426eaffe52734
kdg_win <alsshsskssl@gmail.com>
2025-07-29 12:18:09 +0900

    불필요한 테스트코드,파일 제거

--- ium/src/main/java/com/example/ium/recommend/domain/service/RecommendationStrategyFactory.java
+++
@@ -1,2 +0,0 @@
-// 이 파일은 사용되지 않아 제거되었습니다.
-// RecommendController에서 직접 GptRecommendationService를 사용하므로 Strategy 패턴이 불필요함
--- ium/src/main/java/com/example/ium/recommend/domain/service/WorkRequestDataCollectionService.java
+++
@@ -1,2 +0,0 @@
-// 이 파일은 사용되지 않아 제거되었습니다.
-// ExpertRecommendationStrategy에서만 사용되었으나 해당 Strategy가 제거되어 불필요함
